00b9e94767a57de73e7c7d2d62de3e052faa09a2
ruslan4432013 <ruslan4432013@gmail.com>
2021-12-08 16:40:29 +0300

    add validation email and edit profile

--- geekshop_v2-project/authapp/forms.py
+++ geekshop_v2-project/authapp/forms.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.forms import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.forms import HiddenInput, ValidationError
 
 from .models import ShopUser
+
 
 class ShopUserLoginForm(AuthenticationForm):
     class Meta:
@@ -28,6 +29,32 @@ class ShopUserRegisterForm(UserCreationForm):
     def clean_age(self):
         data = self.cleaned_data['age']
         if data < 18:
-            raise forms.ValidationError('Вы слишком молоды!')
+            raise ValidationError('Вы слишком молоды!')
+        return data
+
+    def clean_email(self):
+        email_valid = self.cleaned_data['email']
+        if email_valid.split('@')[1] != 'gmail.com':
+            raise ValidationError('Ваша почта не от гугл')
+        return email_valid
+
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'email', 'age', 'avatar', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+            if field_name == 'password':
+                field.widget = HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise ValidationError('Вы слишком молоды!')
 
         return data
--- geekshop_v2-project/authapp/views.py
+++ geekshop_v2-project/authapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 from django.contrib import auth
 from django.urls import reverse
 
@@ -41,4 +41,15 @@ def register(request):
     return render(request, 'authapp/register.html', content)
 
 def edit(request):
-    return HttpResponseRedirect(reverse('main'))
+    title = 'Редактирование'
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    content = {'title': title, 'edit_form': edit_form}
+
+    return render(request, 'authapp/edit.html', content)
