889f97e3fdcb92deeac93eecfce14a0a17f563be
ruslan4432013 <ruslan4432013@gmail.com>
2021-12-13 19:18:28 +0300

    added the output of products by category

--- geekshop_v2-project/mainapp/views.py
+++ geekshop_v2-project/mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, ProductCategory
 
 menu = [
@@ -11,21 +11,36 @@ menu = [
 # Create your views here.
 
 def products(request, pk=None):
-    title = "Продукты"
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
-    products = Product.objects.all().order_by('price')
+    print(pk)
+
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+    same_products = Product.objects.all()[3:5]
+
     content = {
-        "title": title,
-        "links_menu": links_menu,
-        "products": products,
-        "menu": menu,
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
     }
+
     return render(request, 'mainapp/products.html', content)
 
 
