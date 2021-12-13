e83781ad075b90e2147269414cc214c91e6bb579
ruslan4432013 <ruslan4432013@gmail.com>
2021-12-13 19:20:12 +0300

    added the output of products by category

---
+++ geekshop_v2-project/basketapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+from mainapp.models import Product
+
+# Create your models here.
+class Basket(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(verbose_name='количество', default=0)
+    add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product.name}, ({self.quantity})'
---
+++ geekshop_v2-project/basketapp/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from basketapp.models import Basket
+from mainapp.models import Product
+
+
+def basket(request):
+    pass
+
+
+def basket_add(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    basket = Basket.objects.filter(user=request.user, product=product).first()
+
+    if not basket:
+        basket = Basket(user=request.user, product=product)
+
+    basket.quantity += 1
+    basket.save()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))  # Возвращает на ту же страницу, где находится
+
+
+def basket_remove(request, pk):
+    pass
